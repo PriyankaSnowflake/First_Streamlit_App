d17481ae9ff142a0e07cc3edd75eca54ee9c14a7
Priyanka <120080200+PriyankaSnowflake@users.noreply.github.com>
2022-12-08 11:59:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,6 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🍲Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣🥑Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔🍞Hard-Boiled Free-Range Egg')
+streamlit.text('Avacardo toast')
 
+streamlit.header('Build your own fruit smoothie')
